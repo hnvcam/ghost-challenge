ab0b90dd31652ad5616012618e3ad72c4dc0b3fc
cam.nguyenhoangvi <hnvcam@gmail.com>
2022-08-25 11:06:13 +0700

    Switch output to docs in order to use github pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const CopyPlugin = require('copy-webpack-plugin')
 
-const dist = path.resolve(__dirname, 'dist')
+const dist = path.resolve(__dirname, 'docs')
 
 module.exports = {
   mode: 'development',
